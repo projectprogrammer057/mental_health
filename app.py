d0ea52d692f6499c6bcdb6f24efd02cd25d4f44f
KALAM SARDAR <78305849+projectprogrammer057@users.noreply.github.com>
2021-04-21 12:38:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('mental.pkl', 'rb'))
+model = pickle.load(open('disorder.pkl', 'rb'))
 
 @app.route('/')
 def home():
